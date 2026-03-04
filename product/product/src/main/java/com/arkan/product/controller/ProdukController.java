146116e0bf67495ad350ae91cf14bb2648d02798
arkanauvanabil <arkanauva.nabil@gmail.com>
2026-03-04 12:07:47 +0700

    update product controller

--- product/product/src/main/java/com/arkan/product/controller/ProdukController.java
+++ product/product/src/main/java/com/arkan/product/controller/ProdukController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,13 @@ public class ProdukController {
     public List<Produk> getAllProduk(){
         return produkService.getAllProduks();
     }
+
+    @PostMapping
+    public Produk createProduk(@RequestBody Produk produk){
+    return produkService.createProduk(produk);
+    }
+
+    
     @GetMapping("/(id)")
     public ResponseEntity<Produk> getProdukById(@PathVariable Long id){
         Produk produk = produkService.getProdukByid(id);
