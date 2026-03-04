1a1ceba139e87dda5399c725d5d61cc9638b8b85
arkanauvanabil <arkanauva.nabil@gmail.com>
2026-03-04 12:00:29 +0700

    first commit

---
+++ pelanggan/pelanggan/src/main/java/arkan/pelanggan/Repository/PelangganRepository.java
@@ -0,0 +1,11 @@
+package arkan.pelanggan.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import arkan.pelanggan.model.Pelanggan;
+
+@Repository
+public interface PelangganRepository extends JpaRepository<Pelanggan, Long>{
+
+}
